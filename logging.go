711905ab995b68495aefd52d3dd7a930c4a08e99
Elio Bischof <elio@zitadel.com>
2024-11-28 23:26:42 +0100

    feat: use slog only

--- logging.go
+++ logging.go
@@ -1,17 +1,19 @@
 package logging
 
 import (
+	"context"
 	"fmt"
+	"log/slog"
 	"runtime"
+	"strings"
 	"time"
-
-	"github.com/sirupsen/logrus"
 )
 
 type Entry struct {
-	*logrus.Entry
-	isOnError bool
-	err       error
+	attributes []any
+	isOnError  bool
+	err        error
+	ts         time.Time
 }
 
 var idKey = "logID"
@@ -23,30 +25,25 @@ func SetIDKey(key string) {
 
 // Deprecated: Log creates a new entry with an id
 func Log(id string) *Entry {
-	entry := (*logrus.Logger)(log).WithField(idKey, id)
-	entry.Logger = (*logrus.Logger)(log)
-	return &Entry{Entry: entry}
+	return &Entry{attributes: []any{slog.String(idKey, id)}}
 }
 
 // Deprecated: LogWithFields creates a new entry with an id and the given fields
 func LogWithFields(id string, fields ...interface{}) *Entry {
-	e := Log(id)
-	return e.SetFields(fields...)
+	return Log(id).SetFields(fields...)
 }
 
 // New instantiates a new entry
 func New() *Entry {
-	return &Entry{Entry: logrus.NewEntry((*logrus.Logger)(log))}
+	return &Entry{}
 }
 
 func OnError(err error) *Entry {
-	e := New()
-	return e.OnError(err)
+	return New().OnError(err)
 }
 
 func WithError(err error) *Entry {
-	e := New()
-	return e.WithError(err)
+	return New().WithError(err)
 }
 
 // WithFields creates a new entry without an id and the given fields
@@ -57,6 +54,9 @@ func WithFields(fields ...interface{}) *Entry {
 // OnError sets the error. The log will only be printed if err is not nil
 func (e *Entry) OnError(err error) *Entry {
 	e.err = err
+	if err != nil {
+		e.attributes = append(e.attributes, slog.Any("err", err))
+	}
 	e.isOnError = true
 	return e
 }
@@ -64,277 +64,302 @@ func (e *Entry) OnError(err error) *Entry {
 // SetFields sets the given fields on the entry. It panics if length of fields is odd
 func (e *Entry) SetFields(fields ...interface{}) *Entry {
 	logFields := toFields(fields...)
-	return e.WithFields(logFields)
-}
-
-func (e *Entry) WithField(key string, value interface{}) *Entry {
-	e.Entry = e.Entry.WithField(key, value)
+	e.attributes = logFields
 	return e
 }
 
-func (e *Entry) WithFields(fields logrus.Fields) *Entry {
-	e.Entry = e.Entry.WithFields(fields)
+func (e *Entry) WithField(key string, value interface{}) *Entry {
+	return e.WithFields(map[string]interface{}{key: value})
+}
+
+func (e *Entry) WithFields(fields map[string]interface{}) *Entry {
+	for k, v := range fields {
+		e.attributes = append(e.attributes, slog.Any(k, v))
+	}
 	return e
 }
 
 func (e *Entry) WithError(err error) *Entry {
-	e.Entry = e.Entry.WithError(err)
+	e.err = err
+	if err != nil {
+		e.attributes = append(e.attributes, slog.Any("err", err))
+	}
 	return e
 }
 
 func (e *Entry) WithTime(t time.Time) *Entry {
-	e.Entry = e.Entry.WithTime(t)
+	e.ts = t
 	return e
 }
 
-func toFields(fields ...interface{}) logrus.Fields {
+func toFields(fields ...interface{}) []any {
 	if len(fields)%2 != 0 {
-		return logrus.Fields{"oddFields": len(fields)}
+		return []any{slog.Int("oddFields", len(fields))}
 	}
-	logFields := make(logrus.Fields, len(fields)%2)
+	logFields := make([]any, 0, len(fields)/2)
 	for i := 0; i < len(fields); i = i + 2 {
 		key := fields[i].(string)
-		logFields[key] = fields[i+1]
+		logFields = append(logFields, slog.Any(key, fields[i+1]))
 	}
 	return logFields
 }
 
 func Debug(args ...interface{}) {
-	e := New()
-	e.log(func() { e.Entry.Debug(args...) })
+	Debugln(args...)
 }
 
 func (e *Entry) Debug(args ...interface{}) {
-	e.log(func() { e.Entry.Debug(args...) })
+	e.Debugln(args...)
 }
 
 func Debugln(args ...interface{}) {
-	e := New()
-	e.log(func() { e.Entry.Debugln(args...) })
+	New().Debugln(args...)
 }
 
 func (e *Entry) Debugln(args ...interface{}) {
-	e.log(func() { e.Entry.Debugln(args...) })
+	msg, attrs := slogArgs(args)
+	e.log(func() { slog.Debug(msg, append(e.attributes, attrs...)...) })
 }
 
 func Debugf(format string, args ...interface{}) {
-	e := New()
-	e.log(func() { e.Entry.Debugf(format, args...) })
+	New().Debugf(format, args...)
 }
 
 func (e *Entry) Debugf(format string, args ...interface{}) {
-	e.log(func() { e.Entry.Debugf(format, args...) })
+	e.log(func() { slog.Debug(slogArgsf(format, args...), e.attributes...) })
 }
 
 func Info(args ...interface{}) {
-	e := New()
-	e.log(func() { e.Entry.Info(args...) })
+	Infoln(args)
 }
 
 func (e *Entry) Info(args ...interface{}) {
-	e.log(func() { e.Entry.Info(args...) })
+	e.Infoln(args)
 }
 
 func Infoln(args ...interface{}) {
-	e := New()
-	e.log(func() { e.Entry.Infoln(args...) })
+	New().Infoln(args...)
 }
 
 func (e *Entry) Infoln(args ...interface{}) {
-	e.log(func() { e.Entry.Infoln(args...) })
+	msg, attrs := slogArgs(args)
+	e.log(func() { slog.Info(msg, append(e.attributes, attrs...)...) })
 }
 
 func Infof(format string, args ...interface{}) {
-	e := New()
-	e.log(func() { e.Entry.Infof(format, args...) })
+	New().Infof(format, args...)
 }
 
 func (e *Entry) Infof(format string, args ...interface{}) {
-	e.log(func() { e.Entry.Infof(format, args...) })
+	e.log(func() { slog.Info(slogArgsf(format, args...), e.attributes...) })
 }
 
 func Trace(args ...interface{}) {
-	e := New()
-	e.log(func() { e.Entry.Trace(args...) })
+	Traceln(args...)
 }
 
 func (e *Entry) Trace(args ...interface{}) {
-	e.log(func() { e.Entry.Trace(args...) })
+	e.Traceln(args...)
 }
 
 func Traceln(args ...interface{}) {
-	e := New()
-	e.log(func() { e.Entry.Traceln(args...) })
+	New().Traceln(args...)
 }
 
 func (e *Entry) Traceln(args ...interface{}) {
-	e.log(func() { e.Entry.Traceln(args...) })
+	msg, attrs := slogArgs(args)
+	e.log(func() { slog.Log(context.Background(), -8, msg, append(e.attributes, attrs...)...) })
 }
 
 func Tracef(format string, args ...interface{}) {
-	e := New()
-	e.log(func() { e.Entry.Tracef(format, args...) })
+	New().Tracef(format, args...)
 }
 
 func (e *Entry) Tracef(format string, args ...interface{}) {
-	e.log(func() { e.Entry.Tracef(format, args...) })
+	e.log(func() { slog.Log(context.Background(), -8, slogArgsf(format, args...), e.attributes...) })
 }
 
 func Warn(args ...interface{}) {
-	e := New()
-	e.log(func() { e.Entry.Warn(args...) })
+	Warnln(args...)
 }
 
 func (e *Entry) Warn(args ...interface{}) {
-	e.log(func() { e.Entry.Warn(args...) })
+	e.Warnln(args...)
 }
 
 func Warnln(args ...interface{}) {
-	e := New()
-	e.log(func() { e.Entry.Warnln(args...) })
+	New().Warnln(args...)
 }
 
 func (e *Entry) Warnln(args ...interface{}) {
-	e.log(func() { e.Entry.Warnln(args...) })
+	msg, attrs := slogArgs(args)
+	e.log(func() { slog.Warn(msg, append(e.attributes, attrs...)...) })
 }
 
 func Warnf(format string, args ...interface{}) {
-	e := New()
-	e.log(func() { e.Entry.Warnf(format, args...) })
+	New().Warnf(format, args...)
 }
 
 func (e *Entry) Warnf(format string, args ...interface{}) {
-	e.log(func() { e.Entry.Warnf(format, args...) })
+	e.log(func() { slog.Warn(slogArgsf(format, args...), e.attributes...) })
 }
 
 func Warning(args ...interface{}) {
-	e := New()
-	e.log(func() { e.Entry.Warning(args...) })
+	Warn(args...)
 }
 
 func (e *Entry) Warning(args ...interface{}) {
-	e.log(func() { e.Entry.Warning(args...) })
+	e.Warn(args...)
 }
 
 func Warningln(args ...interface{}) {
-	e := New()
-	e.log(func() { e.Entry.Warningln(args...) })
+	Warnln(args...)
 }
 
 func (e *Entry) Warningln(args ...interface{}) {
-	e.log(func() { e.Entry.Warningln(args...) })
+	e.Warnln(args...)
 }
 
 func Warningf(format string, args ...interface{}) {
-	e := New()
-	e.log(func() { e.Entry.Warningf(format, args...) })
+	Warnf(format, args...)
 }
 
 func (e *Entry) Warningf(format string, args ...interface{}) {
-	e.log(func() { e.Entry.Warningf(format, args...) })
+	e.Warnf(format, args...)
 }
 
 func Error(args ...interface{}) {
-	e := New()
-	e.log(func() { e.Entry.Error(args...) })
+	New().Error(args...)
 }
 
 func (e *Entry) Error(args ...interface{}) {
-	e.log(func() { e.Entry.Error(args...) })
+	e.Errorln(args...)
 }
 
 func Errorln(args ...interface{}) {
-	e := New()
-	e.log(func() { e.Entry.Errorln(args...) })
+	New().Errorln(args...)
+}
+
+type customErrorFormatter struct {
+	next slog.Handler
+}
+
+func (c customErrorFormatter) Enabled(ctx context.Context, level slog.Level) bool {
+	return level >= slog.LevelError
+}
+
+func (c customErrorFormatter) Handle(ctx context.Context, record slog.Record) error {
+	var attrErr error
+	record.Attrs(func(attr slog.Attr) bool {
+		if attr.Key == "err" {
+			var isErr bool
+			if attrErr, isErr = attr.Value.Any().(error); isErr {
+				return false
+			}
+			return false
+		}
+		return true
+	})
+	if attrErr == nil {
+		return c.next.Handle(ctx, record)
+	}
+	newRecord := record.Clone()
+	newRecord.AddAttrs(
+		slog.String("severity", strings.ToUpper(record.Level.String())),
+		slog.Any("message", fmt.Errorf("%s: %w", record.Message, attrErr)),
+		slog.String("@type", "type.googleapis.com/google.devtools.clouderrorreporting.v1beta1.ReportedErrorEvent"),
+		slog.String("stack_trace", generateStackTrace(record.PC, 3)),
+	)
+	return c.next.Handle(ctx, newRecord)
+}
+
+func generateStackTrace(pc uintptr, skip int) string {
+	var stack []string
+	if fn := runtime.FuncForPC(pc); fn != nil {
+		file, line := fn.FileLine(pc)
+		stack = append(stack, fmt.Sprintf("%s:%d %s", file, line, fn.Name()))
+	}
+	for i := skip; ; i++ {
+		pc, file, line, ok := runtime.Caller(i)
+		if !ok {
+			break
+		}
+		fn := runtime.FuncForPC(pc)
+		stack = append(stack, fmt.Sprintf("%s:%d %s", file, line, fn.Name()))
+	}
+	return strings.Join(stack, "\n")
+}
+
+func (c customErrorFormatter) WithAttrs(attrs []slog.Attr) slog.Handler {
+	return customErrorFormatter{c.next.WithAttrs(attrs)}
+}
+
+func (c customErrorFormatter) WithGroup(name string) slog.Handler {
+	return customErrorFormatter{c.next.WithGroup(name)}
 }
 
 func (e *Entry) Errorln(args ...interface{}) {
-	e.log(func() { e.Entry.Errorln(args...) })
+	e.log(func() {
+		msg, attrs := slogArgs(args)
+		slog.New(customErrorFormatter{next: slog.Default().Handler()}).Error(msg, append(e.attributes, attrs...)...)
+	})
 }
 
 func Errorf(format string, args ...interface{}) {
-	e := New()
-	e.log(func() { e.Entry.Errorf(format, args...) })
+	New().Errorf(format, args...)
 }
 
 func (e *Entry) Errorf(format string, args ...interface{}) {
-	e.log(func() { e.Entry.Errorf(format, args...) })
+	e.log(func() { slog.Error(slogArgsf(format, args...), e.attributes...) })
 }
 
 func Fatal(args ...interface{}) {
-	e := New()
-	e.log(func() { e.Entry.Fatal(args...) })
+	New().Fatal(args...)
 }
 
 func (e *Entry) Fatal(args ...interface{}) {
-	e.log(func() { e.Entry.Fatal(args...) })
+	e.Fatalln(args...)
 }
 
 func Fatalln(args ...interface{}) {
-	e := New()
-	e.log(func() { e.Entry.Fatalln(args...) })
+	New().Fatalln(args...)
 }
 
 func (e *Entry) Fatalln(args ...interface{}) {
-	e.log(func() { e.Entry.Fatalln(args...) })
+	msg, attrs := slogArgs(args)
+	e.log(func() { slog.Log(context.Background(), 12, msg, append(e.attributes, attrs...)...) })
 }
 
 func Fatalf(format string, args ...interface{}) {
-	e := New()
-	e.log(func() { e.Entry.Fatalf(format, args...) })
+	New().Fatalf(format, args...)
 }
 
 func (e *Entry) Fatalf(format string, args ...interface{}) {
-	e.log(func() { e.Entry.Fatalf(format, args...) })
+	e.log(func() { slog.Log(context.Background(), 12, slogArgsf(format, args...), e.attributes...) })
 }
 
 func Panic(args ...interface{}) {
-	e := New()
-	e.log(func() { e.Entry.Panic(args...) })
+	Fatal(args...)
 }
 
 func (e *Entry) Panic(args ...interface{}) {
-	e.log(func() { e.Entry.Panic(args...) })
+	e.Fatal(args...)
 }
 
 func Panicln(args ...interface{}) {
-	e := New()
-	e.log(func() { e.Entry.Panicln(args...) })
+	Fatalln(args...)
 }
 
 func (e *Entry) Panicln(args ...interface{}) {
-	e.log(func() { e.Entry.Panic(args...) })
+	e.Fatalln(args...)
 }
 
 func Panicf(format string, args ...interface{}) {
-	e := New()
-	e.log(func() { e.Entry.Panicf(format, args...) })
+	Fatalf(format, args...)
 }
 
 func (e *Entry) Panicf(format string, args ...interface{}) {
-	e.log(func() { e.Entry.Panicf(format, args...) })
-}
-
-func (e *Entry) Log(level logrus.Level, args ...interface{}) {
-	e.log(func() { e.Entry.Log(level, args...) })
-}
-
-func Logf(level logrus.Level, format string, args ...interface{}) {
-	e := New()
-	e.log(func() { e.Entry.Logf(level, format, args...) })
-}
-
-func (e *Entry) Logf(level logrus.Level, format string, args ...interface{}) {
-	e.log(func() { e.Entry.Logf(level, format, args...) })
-}
-
-func Logln(level logrus.Level, args ...interface{}) {
-	e := New()
-	e.log(func() { e.Entry.Logln(level, args...) })
-}
-
-func (e *Entry) Logln(level logrus.Level, args ...interface{}) {
-	e.log(func() { e.Entry.Logln(level, args...) })
+	e.Fatalf(format, args...)
 }
 
 func (e *Entry) log(log func()) {
@@ -350,10 +375,10 @@ func (e *Entry) checkOnError() *Entry {
 	if !e.isOnError {
 		return e
 	}
-	if e.err != nil {
-		return e.WithError(e.err)
+	if e.err == nil {
+		return nil
 	}
-	return nil
+	return e
 }
 
 func addCaller(e *Entry) {
@@ -362,3 +387,14 @@ func addCaller(e *Entry) {
 		e.WithField("caller", fmt.Sprintf("%s:%d", file, no))
 	}
 }
+
+func slogArgs(args []interface{}) (string, []interface{}) {
+	msg, ok := args[0].(string)
+	if !ok {
+		msg = fmt.Sprintf("%+v", args[0])
+		args = append(args, "nonstringloggingkey", fmt.Sprintf("%T", args[0]))
+	}
+	return msg, args[1:]
+}
+
+var slogArgsf = fmt.Sprintf
